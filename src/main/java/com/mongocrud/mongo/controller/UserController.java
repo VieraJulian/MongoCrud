e4069894b4cc7a33c38f5b02ab733f9d1eb57cbe
VieraJulian <julianviera2019@hotmail.com>
2024-06-02 10:43:04 -0300

    Modificar usuarios mongodb

--- src/main/java/com/mongocrud/mongo/controller/UserController.java
+++ src/main/java/com/mongocrud/mongo/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,23 @@ public class UserController {
         try {
             List<User> users = userRepo.findAll();
             return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable String id, @RequestBody User userInfo) {
+        try {
+            User user = userRepo.findById(id).orElse(null);
+
+            user.setName(userInfo.getName());
+            user.setEmail(userInfo.getEmail());
+
+            User userUpdated = userRepo.save(user);
+
+            return new ResponseEntity<User>(userUpdated, HttpStatus.OK);
 
         } catch (Exception e) {
             return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
