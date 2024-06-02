f7c7e21fbcf2d221a64e66830f50e256f3336313
VieraJulian <julianviera2019@hotmail.com>
2024-06-02 11:21:41 -0300

    Eliminar usuarios mongodb

--- src/main/java/com/mongocrud/mongo/controller/UserController.java
+++ src/main/java/com/mongocrud/mongo/controller/UserController.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,6 +68,17 @@ public class UserController {
             User userUpdated = userRepo.save(user);
 
             return new ResponseEntity<User>(userUpdated, HttpStatus.OK);
+
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("id") String id) {
+        try {
+            userRepo.deleteById(id);
+            return new ResponseEntity<String>("User deleted.", HttpStatus.OK);
 
         } catch (Exception e) {
             return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
